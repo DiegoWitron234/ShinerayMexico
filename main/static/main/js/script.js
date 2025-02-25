7b8265317f00d050a5f2f78b298f28610823384f
Diego Alonso Witrón López <maximusdiegoalonsodbz@gmail.com>
2025-02-25 16:11:02 -0600

    Se modificó el inicio y se creó un nuevo carrusel

--- main/static/main/js/script.js
+++ main/static/main/js/script.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.slide');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    let currentIndex = 0;
 
+    function showSlide(index) {
+        const carouselContainer = document.querySelector('.carousel-container');
+        const slideWidth = slides[0].clientWidth;
+        carouselContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+    }
+
+    prevButton.addEventListener('click', function() {
+        currentIndex = (currentIndex === 0) ? slides.length - 1 : currentIndex - 1;
+        showSlide(currentIndex);
+    });
+
+    nextButton.addEventListener('click', function() {
+        currentIndex = (currentIndex === slides.length - 1) ? 0 : currentIndex + 1;
+        showSlide(currentIndex);
+    });
+});
